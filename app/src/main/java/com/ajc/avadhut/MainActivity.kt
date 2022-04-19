6b3145d7abdcfadbb11c0ede0dc2bfce64eb6b6e
avadhutjc <avadhutjc@gmail.com>
2022-04-19 17:59:47 +0530

    Merge branch 'main' of https://github.com/avadhutjc/AvadhutAvadhut into dev1

--- app/src/main/java/com/ajc/avadhut/MainActivity.kt
+++ app/src/main/java/com/ajc/avadhut/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ajc.avadhut
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
